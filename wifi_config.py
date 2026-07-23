99de3508ab5f9fafe871574abbcd43d600047ea0
IV-168 <haejuniv0227@gmail.com>
2026-07-23 10:09:49 +0900

    Create wifi_config.py

---
+++ wifi_config.py
@@ -0,0 +1,7 @@
+# wifi_config.py  ── 와이파이 정보만 따로 담아두는 파일
+# (이 파일을 main.py와 같은 위치에 저장하면, 모든 코드가 같이 씁니다)
+
+WIFI_SSID = "senWiFi_Free"      # 2.4GHz 와이파이 이름 (대소문자 정확히!)
+WIFI_PASSWORD = "dghs!@2021"
+
+#애는 5G가 안 된대...
